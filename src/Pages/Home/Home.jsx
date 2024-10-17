957849ba5533e57333f9e512271b34a69370ed4f
Ateeq ullah <ateequllah5656@gmail.com>
2024-10-17 11:44:42 +0500

    some new files and changes are made

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Layout from "../../Components/Layouts/Layout";
+import "../../Styles/HomeStyle.css";
+import Section1 from './Section1';
+
+
+
 function Home() {
   return (
    <>
    <Layout>
-      <div>Home</div>
+      {/*Home section hero banner*/ }
+      <Section1 />
+
+
    </Layout>
    </>
   )
